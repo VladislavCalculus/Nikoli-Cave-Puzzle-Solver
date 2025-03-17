d568de8e3da96e888da297e45d779e5ebcab117c
VladislavCalculus <tv42.lakei.vladyslav@gmail.com>
2025-03-17 19:31:49 +0200

    Input verification was added

--- Interface.cc
+++ Interface.cc
@@ -2,6 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 
+ //Допоміжна ф-ція за допомогою якої виконується перевірка правильності вводу
+static int countSpaces(char *line) {
+    int counter = 0;
+    while (*line) {
+        if (*line == ' ') {
+            counter++;
+        }
+        line++;
+    }
+    return counter;
+}
+
 //Функція яка буде визиватися в мейні (Запуск)
 void init(){
 	int width, height;
@@ -11,18 +23,37 @@ void init(){
 
     Grid grid(width, height);
     cin.ignore(); //<- цей ігнор потрібен щоб очистити данні з минулого cin
-    
+
     cout << "Enter rows for the grid (separate numbers using space):\nExample: 3 0 0 2 0\n0 - blank cell\nDigit - numbered cell\n";
     for (int i = 0; i < width; i++) {
         char line[255];
         cin.getline(line, 255);
+        if (countSpaces(line) != width-1) { //За допомогою кількості пробілів перевіряю чи вірна кількість елементів введена, якщо ні - користувач має переввести ввід
+        	cout << "Invalid input. Reinput row:" << endl;
+        	i--;
+        	continue;
+        }
 
-        char *num = strtok(line, " ");
-        for (int j = 0; j < height && num != nullptr; j++) {
-            grid.setPoint(i, j, Point(atoi(num)));
-            num = strtok(nullptr, " ");
+        //Внесення точок на поле
+        char *token = strtok(line, " ");
+        for (int j = 0; j < height && token != nullptr; j++) {
+        	int num = atoi(token);
+        	if (num > 0) { //Робимо визначення точки онлі коли в нас число більше 0, адже користувач як 0 вводить пусту клітинку.
+        		grid.setPoint(i, j, Point(num));
+        	}
+            token = strtok(nullptr, " ");
         }
     }
     cout << "\nGrid state:\n";
     grid.show();
+
+    cout << "\nWait... Solving in prosses..." << endl;
+
+    if (solveCave(&grid, 0, 0)) {
+        cout << "\nSolved Grid:\n";
+        grid.show();
+    } else {
+        cout << "\nNo solution found!\n";
+    }
 }
+
